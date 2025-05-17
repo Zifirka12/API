7f05d19f8843d826eaeecff04772872ad2ae8757
Ziflrka <139446425+Zifirka12@users.noreply.github.com>
2025-05-17 15:14:46 +0300

    Add files via upload

--- src/modules/api.js
+++ src/modules/api.js
@@ -1,1 +1,45 @@
+import { COMMENTS_API_URL } from './constants.js';
+import { formatCommentDate } from './utils.js';
 
+export const getComments = () => {
+  return fetch(COMMENTS_API_URL)
+    .then((response) => {
+      if (!response.ok) {
+        if (response.status >= 500) {
+          throw new Error('Сервер сломался, попробуй позже');
+        }
+        throw new Error(`HTTP ${response.status}`);
+      }
+      return response.json();
+    })
+    .then((data) => {
+      return data.comments.map((comment) => ({
+        name: comment.author.name,
+        date: formatCommentDate(comment.date),
+        text: comment.text,
+        likes: 0,
+        isLiked: false,
+      }));
+    });
+};
+
+export const addComment = (name, text) => {
+  return fetch(COMMENTS_API_URL, {
+    method: 'POST',
+    body: JSON.stringify({
+      name,
+      text,
+      forceError: true,
+    }),
+  }).then((response) => {
+    if (!response.ok) {
+      if (response.status >= 500) {
+        throw new Error('Сервер сломался, попробуй позже');
+      }
+      return response.json().then((error) => {
+        throw new Error(error.error || 'Некорректные данные');
+      });
+    }
+    return response.json();
+  });
+};
